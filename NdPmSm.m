19f635de2ed92174b45ddc33295a1ecbd85629bf
AlexanderBazhenov <59746051+AlexanderBazhenov@users.noreply.github.com>
2025-11-04 21:16:52 +0300

    Add files via upload

--- NdPmSm.m
+++ NdPmSm.m
@@ -12,7 +12,7 @@ set (h, "FaceColor", ColorNow);
 %
 hNd145 = text(86.5, 60.5, 'Nd')
 set(hNd145, 'Fontsize', 24)
-hZ60 = text(86.1, 60.5, '145')
+hZ60 = text(86.1, 60.5, '146')
 set(hZ60, 'Fontsize', 20)
 hN86 = text(86.3, 60.2, 'STABLE')
 set(hN86, 'Fontsize', 18)
@@ -36,7 +36,7 @@ hSm146 = text(84.5, 62.5, 'Sm')
 set(hSm146, 'Fontsize', 24)
 hZ62 = text(84.1, 62.5, '146')
 set(hZ62, 'Fontsize', 20)
-hN84 = text(84.3, 65.2, 'STABLE')
+hN84 = text(84.3, 62.2, 'STABLE')
 set(hN84, 'Fontsize', 18)
 
 set(gca, 'Fontsize', 18)
@@ -44,23 +44,23 @@ xlabel('N, число нейтронов')
 ylabel('Z, электрический заряд')
 
 axis on
-xTICKVAL = [53.5 54.5 55.5]
+xTICKVAL = [84.5 85.5 86.5]
 xticks(xTICKVAL)
 xLABEL = xticklabels
-xLABEL{1,1}='53'
-xLABEL{1,2}='54'
-xLABEL{1,3}='55'
+xLABEL{1,1}='84'
+xLABEL{1,2}='85'
+xLABEL{1,3}='86'
 xticklabels(xLABEL)
-yTICKVAL = [42.5 43.5 44.5]
+yTICKVAL = [60.5 61.5 62.5]
 yticks(yTICKVAL)
 yLABEL = yticklabels
-yLABEL{1,1}='42'
-yLABEL{1,2}='43'
-yLABEL{1,3}='44'
+yLABEL{1,1}='60'
+yLABEL{1,2}='61'
+yLABEL{1,3}='62'
 yticklabels(yLABEL)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-filestr = strcat("Mo-Tc-Ru98")
+filestr = strcat("Nd-Pm-Sm146")
 figure_name_out=strcat(filestr,'.png')
 print('-dpng', '-r300', figure_name_out), pwd
 
